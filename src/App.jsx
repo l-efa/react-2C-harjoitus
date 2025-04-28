f7910c4151ab28e03b816c3a55f14249295ca120
l-efa <lefa.sahi@gmail.com>
2025-04-28 14:50:20 +0300

    frontend login

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,89 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import noteService from "./services/notes";
+import loginService from "./services/login";
+import { use } from "react";
 
 function App() {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((response) => {
       setNotes(response);
     });
   }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteappUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("logging in with", username, password);
+
+    try {
+      const user = await loginService.login({ username, password });
+
+      console.log("user: ", user);
+
+      window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user));
+
+      noteService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      setErrorMessage("wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <h1>Login</h1>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+
+  console.log(user);
+
+  const noteForm = () => (
+    <form onSubmit={addNote}>
+      <input value={newNote} onChange={toggleImportanceOf} />
+      <button type="submit">save</button>
+    </form>
+  );
 
   console.log("render", notes.length, "notes");
 
@@ -21,7 +92,6 @@ function App() {
     : notes.filter((note) => note.important === true);
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`;
     const note = notes.find((n) => n.id === id);
     console.log(note);
     const changedNote = { ...note, important: !note.important };
@@ -62,6 +132,13 @@ function App() {
       <div>
         <Notification errorMessage={errorMessage} />
       </div>
+      {!user && loginForm()}
+      {user && (
+        <div>
+          <p>{user.username} logged in</p>
+          {noteForm()}
+        </div>
+      )}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -70,7 +147,7 @@ function App() {
       <ul>
         {notesToShow.map((note) => (
           <Note
-            key={note.id}
+            key={note._id}
             note={note}
             toggleImportance={() => toggleImportanceOf(note.id)}
           />
@@ -81,6 +158,7 @@ function App() {
         setNewNote={setNewNote}
         notes={notes}
         setNotes={setNotes}
+        addNote={addNote}
       />
     </div>
   );
@@ -110,7 +188,7 @@ const Note = ({ note, toggleImportance }) => {
   );
 };
 
-const Form = ({ newNote, setNewNote, notes, setNotes }) => {
+const Form = ({ newNote, setNewNote, notes, setNotes, addNote }) => {
   const postNote = (event) => {
     event.preventDefault();
     const noteObject = {
@@ -118,16 +196,21 @@ const Form = ({ newNote, setNewNote, notes, setNotes }) => {
       important: Math.random() > 0.5,
     };
 
-    axios
-      .post("http://localhost:3001/api/notes", noteObject)
-      .then((response) => {
-        console.log(response);
-        setNotes(notes.concat(response.data));
-        setNewNote("");
-      })
-      .catch((error) => {
-        console.error("Error posting note", error);
-      });
+    noteService.create(noteObject).then((response) => {
+      setNotes(notes.concat(response));
+      setNewNote("");
+    });
+
+    /*axios
+    .post("https://react-3a-harjoitus.onrender.co/api/notes", noteObject)
+    .then((response) => {
+      console.log(response);
+      setNotes(notes.concat(response.data));
+      setNewNote("");
+    })
+    .catch((error) => {
+      console.error("Error posting note", error);
+    });*/
   };
 
   return (
